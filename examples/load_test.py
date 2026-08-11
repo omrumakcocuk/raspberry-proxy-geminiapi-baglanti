bc179c49676465d60cf8408a700445934bea79c0
msariakcali <147554871+msariakcali@users.noreply.github.com>
2026-08-11 09:53:55 +0300

    proxy_update
    Fix default value for proxy URL argument.

--- examples/load_test.py
+++ examples/load_test.py
@@ -194,7 +194,7 @@ async def main() -> None:
     parser.add_argument(
         "--proxy-url",
         default=os.environ.get(
-            "PROXY_URL", "wss://realtime.orbitkidslab.com/ws/live"
+            "PROXY_URL", " "
         ),
     )
     parser.add_argument("--proxy-pid", type=int)
